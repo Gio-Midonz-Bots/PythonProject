6097b2ad5a2100d6960aa65c8e578bae259cb67c
Giovanna Midonz <roboticagiovanna@gmail.com>
2025-10-28 15:33:06 -0300

    update: Desafio - Extrair dados Do Clone da OLX e salva no CSV

---
+++ ExtraindoDadosTabela/ExtrairDadosParaCSV.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import os
+import pyautogui as time
+
+driver = webdriver.Chrome()
+driver.get('https://clone-olx-devaprender.netlify.app')
+time.sleep(3)
+produto = driver.find_elements(By.XPATH, '//*[@id="root"]/div/main/div[2]/div[1]/div[2]/h3')
+preco = driver.find_elements(By.XPATH,'//*[@id="root"]/div/main/div[2]/div[1]/div[2]/div[1]/span')
+
+for produto,preco in zip(produto,preco):
+    with open('Produtos.csv', 'a', encoding='utf-8') as arquivo:
+        arquivo.write(f'{produto.text},{preco.text},{os.linesep}')
+input('')
